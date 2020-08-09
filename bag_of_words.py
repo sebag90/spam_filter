95cf5ecfb03ace797879a5a7fe1648b10f65aee3
seba <seba.g90@gmail.com>
2020-08-09 10:11:18 +0200

    working but not elegant BoW

--- bag_of_words.py
+++ bag_of_words.py
@@ -8,21 +8,21 @@ from nltk.stem.snowball import SnowballStemmer
 
 class BagWords:
 
-    def __init__(self):
+
+    def __init__(self, language):
+        self.language = language
         self.term_matrix = []
         self.stopwords = set()
         self.texts = {}
-        self.matrix_terms = set()
+        self.matrix_terms = []
         
-        self.retrieve_texts()
-        self.collect_stopwords("en")
-        for key in self.texts:
-            self.texts[key] = self.str_2_vec(self.texts[key], "en")
-            self.create_matrix_terms(self.matrix_terms, self.texts[key])
+        self.__retrieve_texts()
+        self.__collect_stopwords()
+        
 
 
     # manual string manipulation
-    def clean_string(self, _input_str):
+    def __clean_string(self, _input_str):
         cln_str = _input_str.replace("[", " ")
         cln_str = cln_str.replace("]", " ")
         cln_str = cln_str.replace("(", " ")
@@ -41,30 +41,29 @@ class BagWords:
 
 
     # retrieve articles
-    def retrieve_texts(self):
+    def __retrieve_texts(self):
         self.texts = {}
         for filename in os.listdir('./input'):
             path = "./input/" + filename
             with open(path, "r") as file:
                 my_string = file.read()
-                cleaned = self.clean_string(my_string)
+                cleaned = self.__clean_string(my_string)
                 self.texts[filename] = cleaned
         
 
     # collect stop words after removing whitespaces and /n
-    def collect_stopwords(self, language):
+    def __collect_stopwords(self):
 
         with open("stopwords-iso.json") as stopwords:
             data = json.load(stopwords)
-        for word in data[language]:
+        for word in data[self.language]:
             newword = word.replace(" ", "")
             newword2 = newword.replace("\n", "")
             self.stopwords.add(newword2.lower())
-
-
+        
 
     # remove stopwords and stem a string
-    def str_2_vec(self, _input_string, language):
+    def __str_2_vec(self, _input_string):
         # extract single words
         splits = _input_string.split()
         cleaned = []
@@ -82,7 +81,7 @@ class BagWords:
                 cleaned.append(something)
         # stem
     
-        stemmer = SnowballStemmer(languages[language])
+        stemmer = SnowballStemmer(languages[self.language])
         for element in cleaned:
             stemmed.append(stemmer.stem(element))
 
@@ -90,25 +89,47 @@ class BagWords:
 
 
     # create matrix term list
-    def create_matrix_terms(self, _matrix_terms, _article):
+    def __create_matrix_terms(self, _matrix_terms, _article):
         for term in _article:
             if term not in _matrix_terms:
-                _matrix_terms.add(term)
-        self.term_matrix = _matrix_terms
+                _matrix_terms.append(term)
+        self.matrix_terms = _matrix_terms
 
 
+    def __calculate_vec(self, key):
+        string_vec = []
+        for single_term in self.matrix_terms:
+            counter = 0
+            for term in self.texts[key]:
+                if single_term == term:
+                    counter = counter + 1
+            string_vec.append(counter)
+        return string_vec
+
+
+    # PUBLIC METHODS---------------------------------------------------------------------------------
+
+
+    def compute_matrix(self):
+        for key in self.texts:
+            self.texts[key] = self.__str_2_vec(self.texts[key])
+            self.__create_matrix_terms(self.matrix_terms, self.texts[key])
+        for key in self.texts:
+            vec = self.__calculate_vec(key)
+            self.term_matrix.append(vec)
+
+        return self.term_matrix
 
     def print_matrix(self):
         for i in self.term_matrix:
             print(i)
+        
     
 
 
 
-
-
-
-
 if __name__ == "__main__":
-    bag = BagWords()
+    bag = BagWords("en")
+    a = bag.compute_matrix()
     bag.print_matrix()
+    

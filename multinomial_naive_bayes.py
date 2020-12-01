2ada2239d1171ded4d1a20bb760dbd54b62f3cac
sebag90 <seba.g90@gmail.com>
2020-12-01 10:11:40 +0100

    naive bayes perfect

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -6,7 +6,6 @@ class MultinomialNaiveBayes:
     def __init__(self):
         self.feature_probabilities = {}
         self.prior = {}
-        self.probabilities = []
 
 
     def fit(self, X, y, alpha=1):
@@ -14,26 +13,31 @@ class MultinomialNaiveBayes:
         y = np.array(y)
 
         classes, counts = np.unique(y, return_counts=True)
-        self.prior = dict(zip(classes, counts/y.shape[0]))
+        self.prior = dict(zip(classes, np.log(counts/y.shape[0])))
 
         for cl in classes:
             class_sum_vec = X[y==cl].sum(axis=0) 
             added_alpha = (X.shape[1] * alpha )
             class_total_sum = class_sum_vec.sum()
             
-            self.feature_probabilities[cl] = (class_sum_vec + alpha) / ( added_alpha + class_total_sum)
+            self.feature_probabilities[cl] = np.log((class_sum_vec + alpha) / ( added_alpha + class_total_sum))
         
 
-    def predict(self, X):
+    def predict_probs(self, X):
         X = np.array(X)
         results = []
 
         for cl in self.prior:
-            res = self.prior[cl] * (self.feature_probabilities[cl] ** X).prod(axis=1)
+            res = (self.feature_probabilities[cl] * X).sum(axis=1) + self.prior[cl]
             results.append(res)
 
         results = np.column_stack(results)
-        self.probabilities = results
+        return results
+
+
+    def predict(self, X):
+        X = np.array(X)
+        results = self.predict_probs(X)
         return np.argmax(results, axis=1)
 
 

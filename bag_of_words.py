33aae17f6ed8703f22dad8732207deb770c64ab0
sebag90 <seba.g90@gmail.com>
2020-12-01 10:46:16 +0100

    BoW better

--- bag_of_words.py
+++ bag_of_words.py
@@ -86,7 +86,7 @@ class BagWords:
                 self.vocabulary = np.append(self.vocabulary, term)
 
 
-    def calculate_vec(self, sentence_vector):
+    def calculate_vec(self, sentence_vector, voc=None):
         """
         given a sentence in the form of a list of stemmed tokens
         this method calculates a term frequency vector based on 
@@ -95,12 +95,15 @@ class BagWords:
         Input: vector of tokens
         Output: term frequency vector
         """
+        if voc == None: 
+            vocabulary = np.array(self.vocabulary) 
+        else:
+            vocabulary = np.array(voc)
 
-        self.vocabulary = np.array(self.vocabulary) 
         sentence_vector = np.array(sentence_vector)
 
-        indexes = [np.where(x == self.vocabulary)[0][0] for x in sentence_vector]
-        result = np.zeros(self.vocabulary.shape)
+        indexes = [np.where(x == vocabulary)[0][0] for x in sentence_vector]
+        result = np.zeros(vocabulary.shape)
 
         for i in indexes:
             result[i] += 1
@@ -188,12 +191,10 @@ class BagWords:
             difference = set(tokens) - set(self.vocabulary)
             to_append = np.zeros((self.matrix.shape[0], len(difference)))
             matrix = np.append(self.matrix, to_append, axis=1)
-
-            self.create_vocabulary(tokens)
-            question_vector = self.calculate_vec(tokens)
-
+            new_voc = list(self.vocabulary) + list(difference)
+            question_vector = self.calculate_vec(tokens, new_voc)
             result = np.matmul(matrix, question_vector)
-        
+
             return np.argmax(result)
 
 

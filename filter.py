296d487272d792bc5853eef61905a4971b1abcde
sebag90 <seba.g90@gmail.com>
2020-12-03 13:54:06 +0100

    cosm

--- filter.py
+++ filter.py
@@ -4,7 +4,6 @@ import json
 import os
 
 
-
 def retrieve_texts(path):
     texts = []
     for filename in os.listdir(path):
@@ -23,7 +22,6 @@ def main():
     y_train = [0 for i in ham] + [1 for j in spam]
     train_data = ham + spam
 
-    
     bag = bw.BagWords("en")
     
     for text in train_data:
@@ -40,14 +38,13 @@ def main():
     X = bag.matrix[len(y_train):]
     
     nb = mnb.MultinomialNaiveBayes()
-
     nb.fit(X_train, y_train)
     results = nb.predict(X)
     
-
-    output = {"spam": [],
-              "not spam": []}
-
+    output = {
+        "spam": [],
+        "not spam": []
+        }
 
     for text, label in zip(to_classify, results):
         if label == 0:
